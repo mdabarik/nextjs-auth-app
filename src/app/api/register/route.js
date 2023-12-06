354fc43a1ef56c77d8d0f0c7ebdd28850d457fd4
mdabarik <mdabarik19@gmail.com>
2023-12-06 18:32:49 +0600

    mongodb and user added

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,11 +1,16 @@
 import { NextResponse } from "next/server";
+import { connectMongoDB } from "../../../../lib/mongodb";
+import User from "../../../../models/users";
+import bcrypt from "bcryptjs";
 
 export async function POST(req) {
     try {
         const { name, email, password } = await req.json();
-        console.log("name", name);
-        console.log("password", password);
-        console.log("email", email);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        await connectMongoDB();
+        await User.create({name, email, password});
+
         return NextResponse.json({ message: "User registered" }, { status: 201 })
     } catch(error) {
         return NextResponse.json({message: "An error occured while registering users"})
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -26,6 +26,7 @@ const RegisterForm = () => {
                     name, email, password
                 })
             })
+            console.log(res);
             if (res.ok) {
                 const form = e.target;
                 form.reset();
@@ -42,11 +43,11 @@ const RegisterForm = () => {
         <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
                 <h1 className="text-xl font-bold my-4">Register</h1>
-                <form className="flex flex-col gap-3">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-3">
                     <input onChange={e => setName(e.target.value)} type="text" placeholder="Full Name" />
                     <input onChange={e => setEmail(e.target.value)} type="text" placeholder="Email Address" />
                     <input onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
-                    <button onSubmit={handleSubmit} className="bg-green-500 text-white font-bold cursor-pointer px-6 py-2">Login</button>
+                    <button type="submit" className="bg-green-500 text-white font-bold cursor-pointer px-6 py-2">Login</button>
 
                     {
                         error ?
