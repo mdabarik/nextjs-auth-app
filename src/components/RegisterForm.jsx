7664f75699b7f6bff7c3f427331384b966e3b69e
mdabarik <mdabarik19@gmail.com>
2023-12-06 18:04:49 +0600

    dashboard created

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
 
-const LoginForm = () => {
+const RegisterForm = () => {
     return (
         <div className="grid place-items-center h-screen">
-            <div className="shadow-lg p-5 rounded-lg border-5-4 border-green-400">
-                <h1 className="text-xl font-bold my-4">Login</h1>
+            <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+                <h1 className="text-xl font-bold my-4">Register</h1>
                 <form className="flex flex-col gap-3">
+                    <input type="text" placeholder="Full Name" />
                     <input type="text" placeholder="Email" />
                     <input type="password" placeholder="Password" />
                     <button className="bg-green-500 text-white font-bold cursor-pointer px-6 py-2">Login</button>
                     <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
                         Error Message
                     </div>
-                    <Link className="text-sm mt-3 text-right" href="/register">{"Don't have an Account?"}
-                    <span className="underline">Register</span>
+                    <Link className="text-sm mt-3 text-right" href="/">{"Already have an Account?"}
+                        <span className="underline">Login</span>
                     </Link>
                 </form>
             </div>
@@ -21,4 +22,4 @@ const LoginForm = () => {
     );
 };
 
-export default LoginForm;
+export default RegisterForm;
